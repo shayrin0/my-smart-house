cbfc987cdd2ffa99d7445433babcbaabaea9fd6b
marzi <afshanimarzieh@gmail.com>
2020-05-13 02:18:00 +0430

    add comments

--- marzieh/hello.py
+++ marzieh/hello.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask 	#import flask
 
-app = Flask(__name__)
+app = Flask(__name__) 		#creat instance of a flask web app
 
-@app.route("/")
-def home():
+@app.route("/") 			#creat first route(path)
+def home():					#return value(will be displayed on web page)
 	return "Hello World!"
 
-if __name__ =="__main__"
+if __name__ =="__main__"	#run the app
+	app.run()
