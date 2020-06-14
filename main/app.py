856577000faca1e2acb0d435433fa3445b46572f
marzi <afshanimarzieh@gmail.com>
2020-06-14 15:40:56 +0430

    I have done task2-about page

--- main/app.py
+++ main/app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+# we are importing flask class
+from flask import Flask 
 
-app = Flask(__name__)
+# creating app variable and settin this to instance 
+#of this flask class
+app = Flask(__name__) 	
 
+@app.route('/') #our route
+@app.route('/home')	
+def hello_world():
+    return '<h1>Hello, World!</h1>'
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-    
+@app.route('/about') 	#our route
+def about():
+    return '<h1>about page</h1>'
 
-if __name__ == "__main__":
-    app.run()
+#run application directly using python 
+#(this conditinal only true if we run this script directly)
+if __name__ == '__main__': 
+	app.run(debug=True)
